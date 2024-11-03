6cfc3d86d770844f4db989e7e4a388107e44f9bf
Dominik Kiss <kissdominik7@gmail.com>
2024-11-03 17:18:44 +0100

    Update footer

--- src/components/view/Footer.tsx
+++ src/components/view/Footer.tsx
@@ -7,8 +7,8 @@ function Footer() {
         <div className='flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0'>
           <div className='text-sm text-center md:text-left'>
             <p>
-              Az oldalon található adatok kizárólag tájékoztató jellegűek. Mindent megteszünk a pontosság érdekében, de
-              nem vállalunk felelősséget az esetleges hibákért vagy elavult információkért.
+              Az oldalon található adatok kizárólag tájékoztató jellegűek. Az oldal üzemeltetője mindent megtesz a
+              pontosság érdekében, de nem vállal felelősséget az esetleges hibákért vagy elavult információkért.
             </p>
             <p className='mt-2'>
               <span className='font-semibold'>Források:</span> allampapir.hu (AKK), frankfurter.app (ECB)
