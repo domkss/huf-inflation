8f1b79a27930252e89ad482096804685c7ce9a06
Dominik Kiss <kissdominik7@gmail.com>
2024-11-24 14:25:56 +0100

    Lets try this option... Amplify supports ISR => Yeah in another universe I guess

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheMaxMemorySize: 0, // Required for Amplify hosting to work properly with ISR
+  trailingSlash: true, // Sometimes required for Amplify
 };
 
 export default nextConfig;
